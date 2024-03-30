185f4d738558188c299bb26154e7dbfb81b3e33f
unknown <x2.0bgj@gmail.com>
2024-03-31 01:24:45 +0300

    initial

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="vue-component">{{ text }}</div>
+    <div class="vue-component">env: {{ env }}</div>
+</template>
+<script>
+export default {
+    name: 'app',
+    data(){
+        return {
+            text: 'Vue component',
+            env: process.env.NODE_ENV
+        }
+    }
+}
+</script>
+<style>
+    .vue-component {
+        color: brown;
+    }
+</style>
---
+++ webpack.config.ts
@@ -0,0 +1,65 @@
+import { ProgressPlugin, Configuration, DefinePlugin } from 'webpack'
+import path from 'path'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const { VueLoaderPlugin } = require('vue-loader')
+
+import type { Configuration as DevServerConfiguration } from 'webpack-dev-server'
+
+type Mode = 'production' | 'development'
+
+interface EnvVariables {
+    mode: Mode
+    port?: number
+}
+
+export default (env: EnvVariables) => {
+
+    const isDev = env.mode === 'development'
+    const config: Configuration = {
+        mode: env.mode ?? 'development',
+        entry: path.resolve(__dirname, 'src', 'main.ts'),
+        output: {
+            path: path.resolve(__dirname, 'build'),
+            filename: 'bandle.[contenthash].js',
+            clean: true,
+        },
+        plugins: [
+            new HtmlWebpackPlugin({template: path.resolve(__dirname, './public', 'index.html'),}),
+            new VueLoaderPlugin(),
+            isDev && new ProgressPlugin(),
+            new DefinePlugin({
+                'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+                // ...
+              }),
+        ],
+        module: {
+            rules: [
+                {
+                    test: /\.ts?$/,
+                    use: 'ts-loader',
+                    exclude: /node_modules/,
+                },
+                {
+                    test: /\.vue?$/,
+                    use: 'vue-loader',
+                },
+                {
+                    test: /\.css?$/,
+                    use: [
+                        'vue-style-loader',
+                        'css-loader'
+                    ]
+                }
+            ]
+        },
+        resolve: {
+            extensions: ['.ts', '.js']
+        },
+        devServer: isDev ? {
+            port: env.port ?? 5000,
+            open: false,
+        } : undefined
+    }
+    return config
+}
